64cbd5b8d6863a1e9f655d446cf9eadc4f7a0dba
TomAltankhuyag <tomharvard17@gmail.com>
2021-11-23 13:53:36 -0700

    Added changes to UI and added profile default picture

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,19 +1,24 @@
 import React, { Component } from "react";
 import Courses from "./courses";
-
+import defaultProfile from "../defaultProfile.jpg"
 class Profile extends React.Component {
   state = { name: "John Doe" };
   render() {
     return (
       <React.Fragment>
-        <h2>{this.state.name}</h2>
-        <button className="btn btn-primary">Connect</button>
+        <view style={{flex:1, flexDirection:'column', float : "right", marginRight: "3%"}} > 
+        <h2 >{this.state.name}</h2>
+        <img src ={defaultProfile} />
+        <h4>Degree - (in progress/completed)</h4>
+        </view>
+        {/* <button className="btn btn-primary">Connect</button> */}
         <br />
+        
         <h4 style={{ float: "left" }}>current courses</h4>
         <span style={{ float: "left", clear: "left" }}>
           <Courses />
         </span>
-        <h4 style={{ float: "right" }}>Degree - (in progress/completed)</h4>
+       
       </React.Fragment>
     );
   }
