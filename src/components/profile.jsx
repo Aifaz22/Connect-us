9063c9687759d295d29baf989e3faac816a88c3a
TomAltankhuyag <60277488+TomAltankhuyag@users.noreply.github.com>
2021-11-25 21:17:56 -0700

    Profile fix

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,25 +1,32 @@
 import React, { Component } from "react";
 import Courses from "./courses";
-import defaultProfile from "../defaultProfile.jpg"
+import pictureUofC from "../UofCLogo.jpg"
 class Profile extends React.Component {
   state = { name: "John Doe" };
   render() {
     return (
       <React.Fragment>
         <div>{/* test */}
-        <view style={{flex:1, flexDirection:'column', float : "right", marginRight: "3%"}} > 
+        <view style={{flex:1, flexDirection:'column',  marginRight: "3%",}} > 
         <h2 >{this.state.name}</h2>
-        <img src ={defaultProfile} />
+        <img style= {{height: "15%", width: "15%"}} src ={pictureUofC} />
         <h4>Degree - (in progress/completed)</h4>
+        <h2 style = {{marginTop: "1.5%",}}>Major / Minor
 
-        <div style ={{position:"relative", marginRight :"10%"}}>
+        <p style = {{fontSize: "15px", marginTop: "1.5%"}}>Context for {this.state.name} </p>
+        </h2>
+        <h2 style = {{float: "right", marginRight: "5%", marginTop: "-5%"}}>Completed Courses</h2>
 
-        <h4 style={{ float: "right", marginTop: "3%", marginRight: "15%"}}>Enrolled Courses</h4>
-        <span style={{float:"right", clear: "left", fontFamily: "Titillium Web", fontWeight: "700", marginTop: "15%", marginRight: "-35%"}}>
+        <div style ={{position:"relative", marginRight :"10%",marginTop: "-5%", marginLeft: "5%", float: "left"}}>
+        <h2 >Enrolled Courses
+        <span style={{ clear: "left", fontFamily: "Titillium Web", fontWeight: "700", marginTop: "15%", fontSize: "20px"}}>
           <Courses />
         </span>
+        </h2>
+        
         </div>
         </view>
+       
         </div>
 
 
